63f941b2af928101d921319355efbf4d46ce1286
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-09 16:13:35 +0000

    App renders without an error on green

--- src/App.test.js
+++ src/App.test.js
@@ -7,5 +7,23 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() } );
 
 test('renders without an error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
 
 });
